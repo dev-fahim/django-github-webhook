9755ee8888c9138c4f51c957eecfa94f2967bacd
Fahim Al Islam <fahim6668@gmail.com>
2020-07-21 02:35:28 +0600

    Might be fixed

--- webhook/models.py
+++ webhook/models.py
@@ -16,7 +16,7 @@ class WebHookManager(models.Manager):
             event_name=str(request.headers.get('X-GitHub-Event')).title(),
             payloads=body,
             repository_name=body_json['repository']['name'],
-            repository_owner_name=body_json['repository']['owner']['name'],
+            repository_owner_name=body_json['repository']['owner']['login'],
             repository_url=body_json['repository']['url'],
             sender_login=body_json['sender']['login'],
         )
@@ -25,7 +25,7 @@ class WebHookManager(models.Manager):
 class WebHook(models.Model):
     event_id = models.UUIDField(editable=False, default=uuid4, verbose_name='Event local ID')
     repository_name = models.CharField(max_length=255)  # ['repository']['name']
-    repository_owner_name = models.CharField(max_length=255)  # ['repository']['owner']['name']
+    repository_owner_name = models.CharField(max_length=255)  # ['repository']['owner']['login']
     repository_url = models.URLField()  # ['repository']['url']
     sender_login = models.CharField(max_length=255, verbose_name='Event by')  # ['sender']['login']
     event_name = models.CharField(max_length=255)
