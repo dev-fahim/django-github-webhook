bbb721aac62bedda695995798d0fa62ab83d1034
Fahim Al Islam <fahim6668@gmail.com>
2020-07-21 03:07:56 +0600

    Might be fixed

--- webhook/api/views.py
+++ webhook/api/views.py
@@ -16,7 +16,7 @@ def process_build(payloads):
     working_directory = os.getcwd()
     project_dir = payloads['repository']['full_name']
     os.chdir(os.path.expanduser('/home/fahim' + '/app/' + project_dir))
-    shell_run = subprocess.run(['source', working_directory + 'build.sh'], capture_output=True)
+    shell_run = subprocess.run(['.', working_directory + 'build.sh'], capture_output=True)
     error_logs = shell_run.stderr.decode('utf-8')
     logs = shell_run.stdout.decode('utf-8')
     returned = shell_run.returncode
