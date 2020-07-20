89186f609a64017c28e652616789411213f1c340
Fahim Al Islam <fahim6668@gmail.com>
2020-07-21 04:38:32 +0600

    Might be fixed

--- webhook/api/views.py
+++ webhook/api/views.py
@@ -15,6 +15,7 @@ from app_build.models import AppBuildRecord, BUILD_STATUS_CHOICES
 
 def process_build(payloads, event_name):
     returned = -1
+    k = "Hello"
 
     working_directory = os.getcwd()
     project_dir = payloads['repository']['full_name']
@@ -24,8 +25,10 @@ def process_build(payloads, event_name):
         return_code=returned,
         build_logs="Not available"
     )
+    k = "Added"
     try:
         obj.save()
+        k = "Saved"
         os.chdir(os.path.expanduser('/home/fahim' + '/app/' + project_dir))
         # /home/fahim/app/dev-fahim/django-github-webhook/build.sh
         shell_run = subprocess.run(['sudo', '/home/fahim/build.sh'], capture_output=True)
@@ -40,12 +43,15 @@ def process_build(payloads, event_name):
             obj.build_logs = logs
         obj.return_code = returned
         os.chdir(working_directory)
+        k = "Done try"
     except:
         obj.build_status = BUILD_STATUS_CHOICES[1][0]
         obj.build_logs = "Can't run the file"
         obj.return_code = returned
         obj.save()
         pprint.pprint("error occurred on running file...")
+        k = "Done except"
+    return k
 
 
 @api_view(['POST'])
@@ -53,9 +59,10 @@ def get_webhook_events(request):
     payloads = request.data
     WebHook.objects.add_record(request)
     event_name = request.headers.get('X-GitHub-Event')
+    k = None
     if event_name in ['push']:
-        process_build(payloads, event_name)
-    return Response(data={'payloads': payloads}, status=status.HTTP_201_CREATED)
+        k = process_build(payloads, event_name)
+    return Response(data={"status": k, 'payloads': payloads}, status=status.HTTP_201_CREATED)
 
 
 class WebHookListAPIView(ListAPIView):
